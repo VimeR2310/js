bf4ad3f4b0f0ab017f2c5e7a79907114596201c5
VimeR2310 <144179822+VimeR2310@users.noreply.github.com>
2023-11-07 16:00:28 +0300

    Update app.js

--- антон/464-484/app.js
+++ антон/464-484/app.js
@@ -1,10 +1,1 @@
-import http from 'http';
 
-http.createServer();
-
-http.createServer((request, response) => {
-	response.setHeader('Cache-Control', 'no-cache');
-	response.statusCode = 200;
-	response.write('hello world');
-	response.end();
-}).listen(3000);
