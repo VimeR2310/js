8e1e31facb753ba2d1e2e1711b433b2b4105a654
VimeR2310 <144179822+VimeR2310@users.noreply.github.com>
2023-11-07 16:01:33 +0300

    Delete антон/DOM10.js

--- антон/DOM10.js
+++
@@ -1,8 +0,0 @@
-let p = document.querySelectorAll('p');
-let button = document.querySelector('button');
-
-button.onclick = () => {
-    p.forEach(function(p,index){
-        p.textContent += index + i;
-    })
-}
