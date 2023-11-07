8ebe02f2b545717628fcf0c80c1ea4ec79aa7e56
VimeR2310 <144179822+VimeR2310@users.noreply.github.com>
2023-11-07 16:04:09 +0300

    Delete антон/DOM7.js

--- антон/DOM7.js
+++
@@ -1,8 +0,0 @@
-let input = document.querySelector('input');
-
-input.onblur = () => {
-    let value = input.value; 
-    let a = value.split('.').reverse(); 
-    let b = a.join('-');
-    input.value = b;
-}
