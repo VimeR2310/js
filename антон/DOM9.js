5c85538d09c1146f594efabea3279488bc44f80b
VimeR2310 <144179822+VimeR2310@users.noreply.github.com>
2023-11-07 16:04:29 +0300

    Delete антон/DOM9.js

--- антон/DOM9.js
+++
@@ -1,9 +0,0 @@
-let input = document.querySelector('input');
-
-input.onblur = () => {
-    a = input.value;
-    arr = a.split('');
-    for (let i = 0; i < arr.length; i++) {
-        input.value = arr[i] == 3 ? 'Есть число': 'Числа нету';
-    }
-}
